1e8a98b5af4fad2684ba5f1a2454e631b1ea716d
hero00 <jobaied543@gmail.com>
2025-08-20 00:48:36 +0600

    added task data to database

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const app=express();
 const port=process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+app.use(cors());
+app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.ASSIGNMENT10_USER}:${process.env.ASSIGNMENT10_PASS}@cluster0.hota77b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.TASK_USER}:${process.env.TASK_PASS}@cluster0.hota77b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 app.get('/',(req,res)=>{
   res.send('assignment-10-server in running')
@@ -21,16 +23,32 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const taskCollection=client.db('taskdb').collection('task');
+    
+    app.get('/task',async(req,res)=>{
+      const result=await taskCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/task',async(req,res)=>{
+      const taskData=req.body;
+      const result=await taskCollection.insertOne(taskData);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
