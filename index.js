5e4b3041e059bc2c258583ff8d3c52b2859afdc4
hero00 <jobaied543@gmail.com>
2025-08-19 23:53:36 +0600

    create server and connect to mongodb

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const port=process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.ASSIGNMENT10_USER}:${process.env.ASSIGNMENT10_PASS}@cluster0.hota77b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+app.get('/',(req,res)=>{
+  res.send('assignment-10-server in running')
+});
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port,()=>{
+    console.log(`assignment server is running on port ${port}`)
+});
