2f41982b9691fb718bf3066a40d3c182102ee1d5
hero00 <jobaied543@gmail.com>
2025-08-21 13:50:19 +0600

    added task data update functionality

--- index.js
+++ index.js
@@ -57,6 +57,21 @@ async function run() {
       res.send(result);
     })
 
+    app.put('/task/:id',async(req,res)=>{
+     const id=req.params.id;
+     const filter={_id:new ObjectId(id)};
+     const updatedData=req.body;
+     const option={
+      upsert:true
+     }
+     const updatedDoc={
+      $set:updatedData
+     };
+
+     const result=await taskCollection.updateOne(filter,updatedDoc,option);
+     res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
